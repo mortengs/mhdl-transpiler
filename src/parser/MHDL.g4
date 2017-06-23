e71a391aab9d44a2d0524b8bce38a15de7f8dcce
Morten <morten_richardt@hotmail.com>
2017-06-24 01:58:32 +0200

    .

--- src/parser/MHDL.g4
+++ src/parser/MHDL.g4
@@ -4,12 +4,13 @@ IDENTIFIER: [a-zA-Z_][a-zA-Z0-9_]*;
 INTEGER: '-'?[0-9]+;
 WS        : [ \n\t\r]+ -> skip;
 
-design: 'DEFINE' IDENTIFIER entity* components connections EOF;
+design: 'DEFINE' IDENTIFIER entity* components connections 'END DEFINE;' EOF;
 
 entity: 'ENTITY LIST:' (IDENTIFIER ('WITH' | 'WITHOUT') 'CONTROL;')+;
 
 components: 'COMPONENT LIST:'
     (IDENTIFIER 'OF' IDENTIFIER';' | IDENTIFIER ':')+
+    'END LIST;'
 ;
 
 connections: 'CONNECTION LIST:' (IDENTIFIER 'CONNECTS TO' connection_end ';')+ 'END LIST;';
--- src/parser/MHDLParser.java
+++ src/parser/MHDLParser.java
@@ -18,7 +18,8 @@ public class MHDLParser extends Parser {
 		new PredictionContextCache();
 	public static final int
 		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, T__6=7, T__7=8, T__8=9, 
-		T__9=10, T__10=11, T__11=12, T__12=13, IDENTIFIER=14, INTEGER=15, WS=16;
+		T__9=10, T__10=11, T__11=12, T__12=13, T__13=14, IDENTIFIER=15, INTEGER=16, 
+		WS=17;
 	public static final int
 		RULE_design = 0, RULE_entity = 1, RULE_components = 2, RULE_connections = 3, 
 		RULE_connection_end = 4;
@@ -27,13 +28,13 @@ public class MHDLParser extends Parser {
 	};
 
 	private static final String[] _LITERAL_NAMES = {
-		null, "'DEFINE'", "'ENTITY LIST:'", "'WITH'", "'WITHOUT'", "'CONTROL;'", 
-		"'COMPONENT LIST:'", "'OF'", "';'", "':'", "'CONNECTION LIST:'", "'CONNECTS TO'", 
-		"'END LIST;'", "','"
+		null, "'DEFINE'", "'END DEFINE;'", "'ENTITY LIST:'", "'WITH'", "'WITHOUT'", 
+		"'CONTROL;'", "'COMPONENT LIST:'", "'OF'", "';'", "':'", "'END LIST;'", 
+		"'CONNECTION LIST:'", "'CONNECTS TO'", "','"
 	};
 	private static final String[] _SYMBOLIC_NAMES = {
 		null, null, null, null, null, null, null, null, null, null, null, null, 
-		null, null, "IDENTIFIER", "INTEGER", "WS"
+		null, null, null, "IDENTIFIER", "INTEGER", "WS"
 	};
 	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
 
@@ -127,7 +128,7 @@ public class MHDLParser extends Parser {
 			setState(15);
 			_errHandler.sync(this);
 			_la = _input.LA(1);
-			while (_la==T__1) {
+			while (_la==T__2) {
 				{
 				{
 				setState(12);
@@ -143,6 +144,8 @@ public class MHDLParser extends Parser {
 			setState(19);
 			connections();
 			setState(20);
+			match(T__1);
+			setState(21);
 			match(EOF);
 			}
 		}
@@ -183,19 +186,19 @@ public class MHDLParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(22);
-			match(T__1);
-			setState(26); 
+			setState(23);
+			match(T__2);
+			setState(27); 
 			_errHandler.sync(this);
 			_la = _input.LA(1);
 			do {
 				{
 				{
-				setState(23);
-				match(IDENTIFIER);
 				setState(24);
+				match(IDENTIFIER);
+				setState(25);
 				_la = _input.LA(1);
-				if ( !(_la==T__2 || _la==T__3) ) {
+				if ( !(_la==T__3 || _la==T__4) ) {
 				_errHandler.recoverInline(this);
 				}
 				else {
@@ -203,11 +206,11 @@ public class MHDLParser extends Parser {
 					_errHandler.reportMatch(this);
 					consume();
 				}
-				setState(25);
-				match(T__4);
+				setState(26);
+				match(T__5);
 				}
 				}
-				setState(28); 
+				setState(29); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
 			} while ( _la==IDENTIFIER );
@@ -250,42 +253,44 @@ public class MHDLParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(30);
-			match(T__5);
-			setState(37); 
+			setState(31);
+			match(T__6);
+			setState(38); 
 			_errHandler.sync(this);
 			_la = _input.LA(1);
 			do {
 				{
-				setState(37);
+				setState(38);
 				_errHandler.sync(this);
 				switch ( getInterpreter().adaptivePredict(_input,2,_ctx) ) {
 				case 1:
 					{
-					setState(31);
-					match(IDENTIFIER);
 					setState(32);
-					match(T__6);
-					setState(33);
 					match(IDENTIFIER);
-					setState(34);
+					setState(33);
 					match(T__7);
+					setState(34);
+					match(IDENTIFIER);
+					setState(35);
+					match(T__8);
 					}
 					break;
 				case 2:
 					{
-					setState(35);
-					match(IDENTIFIER);
 					setState(36);
-					match(T__8);
+					match(IDENTIFIER);
+					setState(37);
+					match(T__9);
 					}
 					break;
 				}
 				}
-				setState(39); 
+				setState(40); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
 			} while ( _la==IDENTIFIER );
+			setState(42);
+			match(T__10);
 			}
 		}
 		catch (RecognitionException re) {
@@ -331,30 +336,30 @@ public class MHDLParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(41);
-			match(T__9);
-			setState(47); 
+			setState(44);
+			match(T__11);
+			setState(50); 
 			_errHandler.sync(this);
 			_la = _input.LA(1);
 			do {
 				{
 				{
-				setState(42);
-				match(IDENTIFIER);
-				setState(43);
-				match(T__10);
-				setState(44);
-				connection_end();
 				setState(45);
-				match(T__7);
+				match(IDENTIFIER);
+				setState(46);
+				match(T__12);
+				setState(47);
+				connection_end();
+				setState(48);
+				match(T__8);
 				}
 				}
-				setState(49); 
+				setState(52); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
 			} while ( _la==IDENTIFIER );
-			setState(51);
-			match(T__11);
+			setState(54);
+			match(T__10);
 			}
 		}
 		catch (RecognitionException re) {
@@ -394,31 +399,31 @@ public class MHDLParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(56); 
+			setState(59); 
 			_errHandler.sync(this);
 			_la = _input.LA(1);
 			do {
 				{
-				setState(56);
+				setState(59);
 				_errHandler.sync(this);
 				switch ( getInterpreter().adaptivePredict(_input,5,_ctx) ) {
 				case 1:
 					{
-					setState(53);
+					setState(56);
 					match(IDENTIFIER);
 					}
 					break;
 				case 2:
 					{
-					setState(54);
+					setState(57);
 					match(IDENTIFIER);
-					setState(55);
-					match(T__12);
+					setState(58);
+					match(T__13);
 					}
 					break;
 				}
 				}
-				setState(58); 
+				setState(61); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
 			} while ( _la==IDENTIFIER );
@@ -436,23 +441,23 @@ public class MHDLParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\22?\4\2\t\2\4\3\t"+
+		"\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\23B\4\2\t\2\4\3\t"+
 		"\3\4\4\t\4\4\5\t\5\4\6\t\6\3\2\3\2\3\2\7\2\20\n\2\f\2\16\2\23\13\2\3\2"+
-		"\3\2\3\2\3\2\3\3\3\3\3\3\3\3\6\3\35\n\3\r\3\16\3\36\3\4\3\4\3\4\3\4\3"+
-		"\4\3\4\3\4\6\4(\n\4\r\4\16\4)\3\5\3\5\3\5\3\5\3\5\3\5\6\5\62\n\5\r\5\16"+
-		"\5\63\3\5\3\5\3\6\3\6\3\6\6\6;\n\6\r\6\16\6<\3\6\2\2\7\2\4\6\b\n\2\3\3"+
-		"\2\5\6@\2\f\3\2\2\2\4\30\3\2\2\2\6 \3\2\2\2\b+\3\2\2\2\n:\3\2\2\2\f\r"+
-		"\7\3\2\2\r\21\7\20\2\2\16\20\5\4\3\2\17\16\3\2\2\2\20\23\3\2\2\2\21\17"+
-		"\3\2\2\2\21\22\3\2\2\2\22\24\3\2\2\2\23\21\3\2\2\2\24\25\5\6\4\2\25\26"+
-		"\5\b\5\2\26\27\7\2\2\3\27\3\3\2\2\2\30\34\7\4\2\2\31\32\7\20\2\2\32\33"+
-		"\t\2\2\2\33\35\7\7\2\2\34\31\3\2\2\2\35\36\3\2\2\2\36\34\3\2\2\2\36\37"+
-		"\3\2\2\2\37\5\3\2\2\2 \'\7\b\2\2!\"\7\20\2\2\"#\7\t\2\2#$\7\20\2\2$(\7"+
-		"\n\2\2%&\7\20\2\2&(\7\13\2\2\'!\3\2\2\2\'%\3\2\2\2()\3\2\2\2)\'\3\2\2"+
-		"\2)*\3\2\2\2*\7\3\2\2\2+\61\7\f\2\2,-\7\20\2\2-.\7\r\2\2./\5\n\6\2/\60"+
-		"\7\n\2\2\60\62\3\2\2\2\61,\3\2\2\2\62\63\3\2\2\2\63\61\3\2\2\2\63\64\3"+
-		"\2\2\2\64\65\3\2\2\2\65\66\7\16\2\2\66\t\3\2\2\2\67;\7\20\2\289\7\20\2"+
-		"\29;\7\17\2\2:\67\3\2\2\2:8\3\2\2\2;<\3\2\2\2<:\3\2\2\2<=\3\2\2\2=\13"+
-		"\3\2\2\2\t\21\36\')\63:<";
+		"\3\2\3\2\3\2\3\2\3\3\3\3\3\3\3\3\6\3\36\n\3\r\3\16\3\37\3\4\3\4\3\4\3"+
+		"\4\3\4\3\4\3\4\6\4)\n\4\r\4\16\4*\3\4\3\4\3\5\3\5\3\5\3\5\3\5\3\5\6\5"+
+		"\65\n\5\r\5\16\5\66\3\5\3\5\3\6\3\6\3\6\6\6>\n\6\r\6\16\6?\3\6\2\2\7\2"+
+		"\4\6\b\n\2\3\3\2\6\7C\2\f\3\2\2\2\4\31\3\2\2\2\6!\3\2\2\2\b.\3\2\2\2\n"+
+		"=\3\2\2\2\f\r\7\3\2\2\r\21\7\21\2\2\16\20\5\4\3\2\17\16\3\2\2\2\20\23"+
+		"\3\2\2\2\21\17\3\2\2\2\21\22\3\2\2\2\22\24\3\2\2\2\23\21\3\2\2\2\24\25"+
+		"\5\6\4\2\25\26\5\b\5\2\26\27\7\4\2\2\27\30\7\2\2\3\30\3\3\2\2\2\31\35"+
+		"\7\5\2\2\32\33\7\21\2\2\33\34\t\2\2\2\34\36\7\b\2\2\35\32\3\2\2\2\36\37"+
+		"\3\2\2\2\37\35\3\2\2\2\37 \3\2\2\2 \5\3\2\2\2!(\7\t\2\2\"#\7\21\2\2#$"+
+		"\7\n\2\2$%\7\21\2\2%)\7\13\2\2&\'\7\21\2\2\')\7\f\2\2(\"\3\2\2\2(&\3\2"+
+		"\2\2)*\3\2\2\2*(\3\2\2\2*+\3\2\2\2+,\3\2\2\2,-\7\r\2\2-\7\3\2\2\2.\64"+
+		"\7\16\2\2/\60\7\21\2\2\60\61\7\17\2\2\61\62\5\n\6\2\62\63\7\13\2\2\63"+
+		"\65\3\2\2\2\64/\3\2\2\2\65\66\3\2\2\2\66\64\3\2\2\2\66\67\3\2\2\2\678"+
+		"\3\2\2\289\7\r\2\29\t\3\2\2\2:>\7\21\2\2;<\7\21\2\2<>\7\20\2\2=:\3\2\2"+
+		"\2=;\3\2\2\2>?\3\2\2\2?=\3\2\2\2?@\3\2\2\2@\13\3\2\2\2\t\21\37(*\66=?";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
